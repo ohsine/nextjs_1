12a2b1beac2420c76fd3018cfbb65817025a136a
ohsine <ohsine@korea.ac.kr>
2022-12-07 23:40:48 +0900

    1207_nextjs
    Link Component Navigation

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Link from "next/link"
+
 function Home() {
     return (
         <div>
             <h1>Home Page</h1>
+            <Link href="/blog">
+                Blog
+            </Link>
+            <Link href="/product">
+                Products
+            </Link>
+            
         </div>
     )
 }
