a7bceb60d5bec50813b4a972a203653c9b69a047
ohsine <ohsine@korea.ac.kr>
2022-12-08 07:16:35 +0900

    1208_nextjs
    라우팅 마지막
    커스텀 404페이지

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 import Link from "next/link"
+import { useRouter } from "next/router";
 
 function Home() {
+
+    const router = useRouter();
+
+    const handleClick = () => {
+        console.log('Placing your order');
+        router.replace('/product');
+    }
+
     return (
         <div>
             <h1>Home Page</h1>
@@ -10,7 +19,9 @@ function Home() {
             <Link href="/product">
                 Products
             </Link>
-            
+            <button onClick={handleClick}>
+                Place Order
+            </button>
         </div>
     )
 }
